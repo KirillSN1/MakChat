a7a1adc2bffdf357671579b5bdda6e4fa3c86f26
LENOVO-YOGA\kirill <kirill1234563456@gmail.com>
2022-11-07 00:03:10 +0300

    fix

--- core/index.ts
+++ core/index.ts
@@ -1,38 +1,37 @@
-import { WebSocketServer } from "ws";
+import { Server } from "ws";
 import Env from "../env";
 import run from "./app/run";
 
 async function main(){
     const server = await run(Env.port,{ debug:Env.DEBUG })
     console.log(`Server started on port ${Env.port}.`);
-    const socket = new WebSocketServer({ server },()=>{
-        console.log(`Web socket started.`);
-        var connections = 0;
-        socket.addListener("connection",(client, request)=>{
-            const index = connections++;
-            console.log(`added connection ${index}`); 
-            client.on("error", (error)=>{
-                console.log(`Error on connection #${index}:`);
-                console.error(error); 
-            });
-            client.on("close", (code, reason)=>{
-                console.log(`Connection #${index} closed with code #${code}:`);
-                console.error(reason);
-            });
-            client.on("message", (data, isBinary)=>{
-                if(data.toString() == "PING") client.send("PONG");
-                console.log(`Message from #${index}:`, data.toString());
-            });
-            client.on("pong",(data)=>{
-                console.log(`Pong from #${index}:`, data);
-            });
-            client.on("unexpected-response",(request, response)=>{
-                console.log(`Unexpected response from #${index}.`);
-            });
-            client.on("upgrade",(request)=>{
-                console.log(`Upgrade on ${index}.`);
-            })
+    const socket = new Server({ server });
+    console.log(`Web socket started.`);
+    var connections = 0;
+    socket.on("connection",(client, request)=>{
+        const index = connections++;
+        console.log(`added connection ${index}`); 
+        client.on("error", (error)=>{
+            console.log(`Error on connection #${index}:`);
+            console.error(error); 
         });
+        client.on("close", (code, reason)=>{
+            console.log(`Connection #${index} closed with code #${code}:`);
+            console.error(reason);
+        });
+        client.on("message", (data, isBinary)=>{
+            if(data.toString() == "PING") client.send("PONG");
+            console.log(`Message from #${index}:`, data.toString());
+        });
+        client.on("pong",(data)=>{
+            console.log(`Pong from #${index}:`, data);
+        });
+        client.on("unexpected-response",(request, response)=>{
+            console.log(`Unexpected response from #${index}.`);
+        });
+        client.on("upgrade",(request)=>{
+            console.log(`Upgrade on ${index}.`);
+        })
     });
 }
 main();
