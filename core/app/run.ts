13b5704c8ec351cf8c2c0143fe15aa523e80a2da
LENOVO-YOGA\kirill <kirill1234563456@gmail.com>
2023-05-13 00:58:06 +0300

    fix https

--- core/app/run.ts
+++ core/app/run.ts
@@ -1,13 +1,20 @@
 import http from "http";
+import https from "https";
 import Router from '../router/Router';
 import Log from '../logs/Log';
+import fs from 'fs';
 import '../../routes/middleware';
 import '../../routes/web';
 import '../../routes/api';
 import '../../routes/ws';
 
 export default function run(port:number,{ debug = false }:any){
-    const server = new http.Server((request, response)=>{
+    const privateKey = fs.readFileSync('privatekey.pem' );
+    const certificate = fs.readFileSync('certificate.pem' );
+    const options = {
+        key:privateKey, cert:certificate
+    }
+    const server = https.createServer(options, (request, response)=>{
         try{ Router.onRequest(request, response) } 
         catch(e:any){
             const now = new Date();
@@ -18,6 +25,6 @@ export default function run(port:number,{ debug = false }:any){
                 if(debug) response.end(`${e.stack}`);
             }
         }
-    });
+    })
     return new Promise<http.Server>((resolve)=>server.listen(port, ()=>resolve(server)));
 }
